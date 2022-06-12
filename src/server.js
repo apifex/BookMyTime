80d25b4374b0ae21d413e3983f0d4ffb467b186a
apifex <apifex@gmail.com>
2022-06-12 11:11:21 +0200

    config for herouku

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use(express.static(path.resolve(__dirname, '../dist')));
 app.listen(process.env.PORT || 5000, function () { return console.log("server liestening on 5000"); });
