82c3889101e5c0c1c615d8f9cb41d66333cd760f
apifex <apifex@gmail.com>
2022-06-12 11:08:59 +0200

    config for herouku

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.resolve(__dirname, '../dist')))
+app.use(express.static(path.resolve(__dirname, 'dist')))
 
-app.listen('5000', () => console.log(`server liestening on 5000`))
+app.listen(process.env.PORT || 5000, () => console.log(`server liestening on 5000`))
